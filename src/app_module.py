53fcbd0e35f52e9d81b323e302fd49d9490b5fbb
Roy Cohen <roei8800@gmail.com>
2024-08-07 07:48:36 +0300

    add bearer token auth

--- src/app_module.py
+++ src/app_module.py
@@ -1,7 +1,13 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
 from nest.core import PyNestFactory, Module
 from .app_controller import AppController
 from .app_service import AppService
 from src.trips.trips_module import TripsModule
+from starlette.requests import Request
+from fastapi import HTTPException
+
+bearer_token = "PU=-r)i7{1ldp-rh(J'B#i\!P.8il2w[D!W*dvH.S2-[9x#vPsBGxol6Xje3-Gza"
 
 @Module(
     imports=[TripsModule],
@@ -19,4 +25,17 @@ app = PyNestFactory.create(
     version="1.0.0",
     debug=True,
 )
+
+@app.http_server.middleware("http")
+async def dispatch(request: Request, call_next):
+    try:
+        auth_header = request.headers.get('Authorization')
+        if not (auth_header is None or auth_header.split(" ")[1] != bearer_token):
+            response = await call_next(request)
+            return response
+        else:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+    except Exception as exc:
+        return JSONResponse(status_code=500, content={'error': str(exc)})
+
 http_server = app.get_server()
