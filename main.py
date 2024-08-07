b77a7ff0ba3094a3210873050cedbbf46de88600
Roy Cohen <roei8800@gmail.com>
2024-08-07 08:36:38 +0300

    change to https

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ if __name__ == '__main__':
     uvicorn.run(
         'src.app_module:http_server',
         host="0.0.0.0",
-        port=8000,
-        reload=True
+        port=443,
+        reload=True,
+        ssl_certfile='src/cert.pem',
+        ssl_keyfile='src/key.pem'
     )
     
